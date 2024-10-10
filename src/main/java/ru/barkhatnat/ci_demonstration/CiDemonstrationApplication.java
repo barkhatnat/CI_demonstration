78443935ed9b17684b372f56a41033989909ace8
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-10-10 22:25:30 +0300

    add project files

---
+++ src/main/java/ru/barkhatnat/ci_demonstration/CiDemonstrationApplication.java
@@ -0,0 +1,13 @@
+package ru.barkhatnat.ci_demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiDemonstrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/barkhatnat/ci_demonstration/CiDemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.barkhatnat.ci_demonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemonstrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
